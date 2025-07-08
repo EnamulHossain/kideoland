22054abd017e8b74cc10943d2faab63910b626a7
enamul <enamulhossain79@gmail.com>
2025-07-08 08:55:54 +0600

    add to cart

--- resources/views/frontend/megamart/partials/last_view_product_box_1.blade.php
+++ resources/views/frontend/megamart/partials/last_view_product_box_1.blade.php
@@ -71,7 +71,7 @@
                 $min_bid_amount = $highest_bid != null ? $highest_bid + 1 : $product->starting_bid;
             @endphp
             <a class="cart-btn absolute-bottom-left w-100 h-35px aiz-p-hov-icon text-white fs-13 fw-700 d-flex flex-column justify-content-center align-items-center @if (in_array($product->id, $cart_added)) active @endif"
-                href="{{ route('cart') }}">
+                href="javascript:void(0)" onclick="bid_single_modal({{ $product->id }}, {{ $min_bid_amount }})">
                 <span class="cart-btn-text">{{ translate('Place Bid') }}</span>
                 <span><i class="las la-2x la-gavel"></i></span>
             </a>
